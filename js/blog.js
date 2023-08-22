e6115f4b6a270d4073708d59a445f0d627e4e51a
LauraKaraliene <Laura.Karaliene@stud.noroff.no>
2023-08-22 21:16:06 +0200

    2023-08-22
    fixed fetched results to blog.html and blog.js pages.

--- js/blog.js
+++ js/blog.js
@@ -1,28 +1,36 @@
 
-
 var page = 1;
+
 async function getMorePosts() {
   page += 1;
-  createHTML();
+  const newUrl = `https://awesomepeaks.no/wp-json/wp/v2/posts?&per_page=5&page=${page}`;
+  await getPosts(newUrl);
 };
 
 
-const url = `https://awesomepeaks.no/wp-json/wp/v2/posts?&per_page=10&page=${page}`;
-
+const url = `https://awesomepeaks.no/wp-json/wp/v2/posts?&per_page=5&page=${page}`;
 const postList = document.querySelector(".post-list");
 const loading = document.querySelector(".loading");
-
+var totalLoadedPosts = 0;
 
 
 
 async function getPosts(url) {
      loading.classList.add("loading");
-    postList.innerHTML = "";
+    // postList.innerHTML = "";
     try {
       const response = await fetch(url);
-      // const response = await fetch(url + `&page=${page}`);
       const results = await response.json();
       console.log(results);
+
+      totalLoadedPosts += results.length;
+
+      if (totalLoadedPosts >= 17) {  
+        document.querySelector(".load-more").style.display = 'none';  
+    } else {
+        document.querySelector(".load-more").style.display = 'block';  
+    }
+  
       
        createHTML(results);
     } catch (error) {
@@ -30,15 +38,14 @@ async function getPosts(url) {
      } finally {
       loading.classList.remove("loading");
      }
-  }
+  };
 
 
-  //image call incorect???
+  //image call 
   async function getImage(imageId) {
     const urlImage = `https://awesomepeaks.no/wp-json/wp/v2/media/${imageId}`;
     const response = await fetch(urlImage);
     const images = await response.json();
-    // console.log(images);
     return images;
     
   };
@@ -46,32 +53,45 @@ async function getPosts(url) {
   getPosts(url);
 
   
-  async function createHTML(posts) {
-     posts.forEach(async function (post) {
-      console.log(post.featured_media);
-      
-      const featuredMedia = await getImage (post.featured_media);
-      console.log(featuredMedia);
-      
-      if (featuredMedia) {
-      const featuredImage = featuredMedia.source_url; 
 
-      postList.innerHTML += `<div>
-                                 <a href="specific.html?post=${post.id}" class="no-underline">
+
+
+  function extractContent(htmlString) {
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(htmlString, 'text/html');
+    const h2 = doc.querySelector('h2') ? doc.querySelector('h2').textContent : null;
+    const h3 = doc.querySelector('h3') ? doc.querySelector('h3').textContent : null;
+
+    return { h2, h3 };
+}
+
+
+
+
+  async function createHTML(posts) {
+    for (const post of posts) {
+      const featuredMedia = await getImage(post.featured_media);
+      const contentHtml = post.content.rendered;
+      const extractedData = extractContent(contentHtml);
+      if (featuredMedia) {
+        const featuredImage = featuredMedia.source_url;
+        postList.innerHTML += `<div>
+                                <a href="specific.html?post=${post.id}" class="no-underline">
                                 <img src="${featuredImage}" 
                                 alt="" class="blog-image"/>
-                                 <h1 class="blog-title">${post.title.rendered}</h1>
-                              
-                                
+                                <h1 class="blog-title">${post.title.rendered}</h1>
+                                <h2>${extractedData.h2}</h2>
+                                <h3>${extractedData.h3}</h3>
                                 </a>
                               </div>`;
       }
-    });
+    }
   }
 
+  document.querySelector(".load-more").addEventListener("click", getMorePosts);
+
  
-  // <h3>${post.content.rendered}</h3>
-  // <h3>${post.excerpt.rendered}</h3>
+
 
 
 
